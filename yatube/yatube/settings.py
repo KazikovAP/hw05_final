c2ad85a614a08d6ce12dab4e1877bbeb45e7c42d
Aleksey Kazikov <kas.alex29@gmail.com>
2023-04-03 17:15:17 +0300

    Сделал админку джанго на сервере. Доработал дизайн сайта. Сделал хорошую цветовую текстуру

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -103,6 +103,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+# STATIC_ROOT = "/home/AleksKazikov/yatube/static"
+
 LOGIN_URL = 'users:login'
 
 LOGIN_REDIRECT_URL = 'posts:index'
