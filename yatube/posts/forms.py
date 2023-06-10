ac538866d93eeb1bb6d2c67c3f3220d5ca1b77a9
Aleksey Kazikov <kas.alex29@gmail.com>
2023-06-10 13:17:13 +0300

    Вроде последние изменения в работе сайта

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -1,28 +1,11 @@
-import pymorphy2
-
 from django import forms
+
+from morf_analiz import lemmatize
 
 from .models import Post, Comment, Group, Preferences
 
-morph = pymorphy2.MorphAnalyzer()
-
 
 class PostForm(forms.ModelForm):
-
-    def lemmatize(self, text):
-        words = text.split()
-        result = []
-        for word in words:
-            p = morph.parse(word)[0]
-            result.append(p.normal_form)
-
-        result1 = []
-        for x in result:
-            if x not in result1:
-                result1.append(x)
-
-        return result1
-
     def clean(self):
         """
         Проверяет, соответствует ли текст поста ключевым словам выбранной группы.
@@ -32,7 +15,7 @@ class PostForm(forms.ModelForm):
         self.errors.clear()
         cleaned_data = super().clean()
         text = cleaned_data.get('text')
-        text = self.lemmatize(text)
+        text = lemmatize(text)
         group = cleaned_data.get('group')
 
         if group and not self.group_matches_text(group, text):
@@ -51,14 +34,9 @@ class PostForm(forms.ModelForm):
         :param text: текст поста
         :return: True, если текст содержит хотя бы одно ключевое слово из группы, иначе False
         """
-        text = self.lemmatize(text)
         k_words_list = group.k_words.split(',')
         new_kwargs = [k_word.strip() for k_word in k_words_list]
-<<<<<<< HEAD
         return bool(list(set(text) & set(new_kwargs)))
-=======
-        return bool(list(set(text.split()) & set(new_kwargs)))
->>>>>>> a18c8179dd614c20037ce33423cb7c295b1a2741
 
     def find_most_matching_group(self, text):
         """
@@ -68,19 +46,11 @@ class PostForm(forms.ModelForm):
         """
         groups = Group.objects.all()
         group_matches = {}
-<<<<<<< HEAD
-        text = self.lemmatize(text)
 
         for group in groups:
             k_words_list = group.k_words.split(',')
-
-=======
-
-        for group in groups:
-            k_words_list = group.k_words.split(',')
->>>>>>> a18c8179dd614c20037ce33423cb7c295b1a2741
             new_kwargs = [k_word.strip() for k_word in k_words_list]
-            count_kwargs = len(list(set(text.split()) & set(new_kwargs)))
+            count_kwargs = len(list(set(text) & set(new_kwargs)))
             if count_kwargs:
                 group_matches[group] = count_kwargs
 
@@ -88,11 +58,7 @@ class PostForm(forms.ModelForm):
             most_matching_group = max(group_matches, key=group_matches.get)
             return most_matching_group
         else:
-<<<<<<< HEAD
-            self.add_error(None, 'Текст поста не соответствует не одной группе. Добавьте пост без группы или в группу "Всякое".')
-=======
-            self.add_error(None, f'Текст поста не соответствует выбранной группе и подходящая группа не найдена.')
->>>>>>> a18c8179dd614c20037ce33423cb7c295b1a2741
+            self.add_error(None, 'Текст поста не соответствует не одной группе. Добавьте пост без группы')
 
     class Meta:
         model = Post
